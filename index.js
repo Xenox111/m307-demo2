8060faffe27c9a7ddcca813c720b637f4c56524a
Yannik 11 <127302114+balthy11@users.noreply.github.com>
2024-09-17 14:44:24 +0000

    Fiu sache created

--- index.js
+++ index.js
@@ -17,6 +17,26 @@ app.get("/impressum", async function (req, res) {
   res.render("impressum", {});
 });
 
+app.get("/createpost", async function (req, res) {
+  res.render("createpost", {});
+});
+
+app.get("/login", async function (req, res) {
+  res.render("login", {});
+});
+
+app.get("/post", async function (req, res) {
+  res.render("post", {});
+});
+
+app.get("/ranking", async function (req, res) {
+  res.render("ranking", {});
+});
+
+app.get("/profile", async function (req, res) {
+  res.render("profile", {});
+});
+
 /* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
 app.listen(3010, () => {
   console.log(`Example app listening at http://localhost:3010`);
