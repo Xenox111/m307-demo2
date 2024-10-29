64158a949320b5cc357362105c6e22739069aa6f
Kamino <fabio.rusca07@hotmail.com>
2024-10-29 15:47:07 +0000

    Oktober1

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import { createApp } from "./config.js";
+import { createApp, upload } from "./config.js";
 
 const app = createApp({
-  user: "autumn_star_7622",
-  host: "168.119.168.41",
-  database: "demo",
-  password: "uaioysdfjoysfdf",
-  port: 18324,
+  user: "twilight_sunset_1970",
+  host: "bbz.cloud",
+  database: "twilight_sunset_1970",
+  password: "fe481b3d0355958b883c084760bebbbe",
+  port: 30211,
 });
 
 /* Startseite */
-app.get("/", async function (req, res) {
+app.get("/start", async function (req, res) {
   res.render("start", {});
 });
 
@@ -17,12 +17,12 @@ app.get("/impressum", async function (req, res) {
   res.render("impressum", {});
 });
 
-app.get("/createpost", async function (req, res) {
-  res.render("createpost", {});
-});
-
 app.get("/login", async function (req, res) {
   res.render("login", {});
+});
+
+app.get("/signin", async function (req, res) {
+  res.render("signin", {});
 });
 
 app.get("/post", async function (req, res) {
@@ -35,6 +35,33 @@ app.get("/ranking", async function (req, res) {
 
 app.get("/profile", async function (req, res) {
   res.render("profile", {});
+});
+
+app.get("/foryou", async function (req, res) {
+  res.render("foryou", {});
+});
+
+app.get("/foryou", async function (reg, res) {
+  const events = await app.locals.pool.query("select * from post");
+  res.render("/foryou", { post: post.rows });
+});
+
+app.get("/new_post", async function (req, res) {
+  res.render("new_post", {});
+});
+
+app.post("/create_post", upload.single("photo_url"), async function (req, res) {
+  const result = await app.locals.pool.query(
+    "INSERT INTO post (title, description, category, photo_url, upload_date, user_id) VALUES ($1, $2, $3, $4, CURRENT_TIMESTAMP, 1)",
+    [
+      req.body.title,
+      req.body.description,
+      req.body.category,
+      req.body.photo_url,
+    ]
+  );
+  console.log(result);
+  res.redirect("/start");
 });
 
 /* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
